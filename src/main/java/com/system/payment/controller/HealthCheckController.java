4a7f4a7ea11b6b0955e0dd54d0edc2146f72898d
jaebin1234 <jaebin1234>
2025-07-22 16:00:45 +0900

    add: health check API 추가

---
+++ src/main/java/com/system/payment/controller/HealthCheckController.java
@@ -0,0 +1,20 @@
+package com.system.payment.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payment")
+public class HealthCheckController {
+	private static final Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
+
+	@GetMapping("/health-check")
+	public ResponseEntity<String> healthCheck() {
+		logger.info("health check called");
+		return ResponseEntity.ok("ok");
+	}
+}
