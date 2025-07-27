c75e038da8aaf73f5cb3132179d3bb178f43047b
jaebin1234 <43032589+jaebin1234@users.noreply.github.com>
2025-07-28 00:07:00 +0900

    fix : init.sql 수정

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,3 +1,11 @@
+-- example 테이블이 없으면 생성
+CREATE TABLE IF NOT EXISTS example (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+GRANT ALL PRIVILEGES ON TABLE example TO manager;
+
 INSERT INTO example (name) VALUES
 ('샘플1'),
 ('샘플2'),
