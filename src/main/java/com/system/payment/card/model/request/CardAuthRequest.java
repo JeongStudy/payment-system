9ec91290be1a2650d4d6db00de876421c95f5693
YUNHWAN <629jyh7@naver.com>
2025-08-01 15:56:12 +0900

    feat: Inicis 요청 파라미터 생성 API - 공통 응답 Interface를 통해 여러 PG사 연동 가능하도록 수정 - Card와 Inicis 서비스 분리를 통해 결합도 감소 - SHA 암호화 메서드 유틸로 분리 - 글로벌 예외 처리추가(IllegalArgumentException)

--- src/main/java/com/system/payment/card/model/request/CardAuthRequest.java
+++ src/main/java/com/system/payment/card/model/request/CardAuthRequest.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 @Getter
 public class CardAuthRequest{
     private String pgCompany;
-    private String buyername;
-    private String buyertel;
-    private String buyeremail;
-    private String goodname;
+    private String buyerName;
+    private String buyerTel;
+    private String buyerEmail;
+    private String goodName;
 }
