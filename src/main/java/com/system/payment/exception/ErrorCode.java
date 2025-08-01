3b0ddbf15f1c6af2ccc4d316708e6cc30fb4ddf8
jaebin1234 <jaebin1234>
2025-08-01 15:54:14 +0900

    add : ErrorCode 수정

--- src/main/java/com/system/payment/exception/ErrorCode.java
+++ src/main/java/com/system/payment/exception/ErrorCode.java
@@ -10,11 +10,19 @@ import lombok.Getter;
 @JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum ErrorCode {
     BAD_REQUEST_PARAM(4000, "Bad Request Parameter"),
-    RSA_KEY_GENERATION_FAIL(5001, "An internal server error occurred during RSA key generation."),
-    SERVER_NULL_POINTER_ERROR(5000, "Null Pointer Error"),
     SERVER_ERROR(5000, "Server Error"),
+    SERVER_NULL_POINTER_ERROR(5001, "Null Pointer Error"),
+    RSA_KEY_GENERATION_FAIL(5002, "An internal server error occurred during RSA key generation."),
+    FAILED_TO_DECRYPT_AES_KEY_WITH_RSA_KEY(5003, "Failed to decrypt AES key with RSA Key."),
+    FAILED_TO_DECRYPT_PASSWORD_WITH_AES_KEY(5003, "Failed to decrypt password with AES key."),
 
-    USER_NOT_EXIST(1001, "사용자가 존재하지 않습니다.");
+
+    USER_NOT_EXIST(1001, "사용자가 존재하지 않습니다."),
+    RSA_KEY_NOT_FOUND(1002, "RSA 키가 존재하지 않습니다."),
+    INVALID_RSA_KEY(1002, "RSA 키가 유효하지 않거나 만료되었습니다."),
+    AES_KEY_NOT_FOUND(1002, "AES 키가 존재하지 않습니다."),
+    INVALID_AES_KEY(1003, "AES 키가 유효하지 않거나 만료되었습니다."),
+    DUPLICATE_EMAIL(1004, "이미 존재하는 이메일입니다.");
 
     private final int status;
     private final String message;
