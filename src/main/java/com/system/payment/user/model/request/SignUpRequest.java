29a795edf75ab8d65f04854b97f0560ab48467fd
jaebin1234 <43032589+jaebin1234@users.noreply.github.com>
2025-08-06 00:21:01 +0900

    fix : 회원가입 요청 SignUpRequest Dto 롬복 어노테이션 추가

--- src/main/java/com/system/payment/user/model/request/SignUpRequest.java
+++ src/main/java/com/system/payment/user/model/request/SignUpRequest.java
@@ -2,11 +2,15 @@ package com.system.payment.user.model.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignUpRequest {
 
 	@NotBlank
@@ -30,4 +34,5 @@ public class SignUpRequest {
 
 	@NotBlank
 	private String phoneNumber;
+
 }
