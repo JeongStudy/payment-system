e579cc8fb2a2bc47a8eda1fd2a3c66e71c8fff49
jaebin1234 <jaebin1234>
2025-08-06 00:09:43 +0900

    add : SuccessCode 추가(로그인 성공) 및 AES_KEY_CREATED 메시지 수정

--- src/main/java/com/system/payment/util/SuccessCode.java
+++ src/main/java/com/system/payment/util/SuccessCode.java
@@ -12,7 +12,9 @@ public enum SuccessCode {
     OK(2000, "요청이 정상적으로 처리되었습니다."),
     USER_CREATED(2001, "회원가입이 완료되었습니다."),
     RAS_KEY_CREATED(2002, "RSA Key가 생성되었습니다."),
-    AES_KEY_CREATED(2003, "AES key가 생성되었습니다.");
+    AES_KEY_CREATED(2003, "AES Key가 생성되었습니다."),
+    USER_LOGIN_SUCCESS(2004, "로그인이 완료되었습니다."),
+    ;
 
     private final int status;
     private final String message;
