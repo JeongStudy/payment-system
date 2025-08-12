9673397e530d13a1c10d58b31376e0c01ebd4a63
YUNHWAN <629jyh7@naver.com>
2025-08-12 16:26:45 +0900

    feat: 카드 API 리팩토링 - oid, timestamp 생성 Util 추가 - pgCompany ENUM으로 관리 - pending 상태 저장 메서드 분리

--- src/main/java/com/system/payment/card/service/InicisService.java
+++ src/main/java/com/system/payment/card/service/InicisService.java
@@ -3,12 +3,14 @@ package com.system.payment.card.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.system.payment.card.domain.BillingKeyStatus;
 import com.system.payment.card.domain.PaymentUserCard;
+import com.system.payment.card.model.request.CardAuthRequest;
 import com.system.payment.card.model.request.InicisRequest;
 import com.system.payment.card.model.response.InicisBillingAuthResponse;
 import com.system.payment.card.model.response.InicisBillingKeyResponse;
 import com.system.payment.card.repository.PaymentUserCardRepository;
 import com.system.payment.user.domain.PaymentUser;
 import com.system.payment.util.HashUtils;
+import com.system.payment.util.IdGeneratorUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
@@ -35,11 +37,11 @@ public class InicisService {
     private final RestTemplate restTemplate;
     private final InicisClient inicisClient;
     private final PaymentUserCardRepository paymentUserCardRepository;
+    private final IdGeneratorUtil idGeneratorUtil;
 
     // 데모 테스트 고정 파라미터
     private String mid = "INIBillTst";
-    private String price = "1000";
-    private long timestamp = System.currentTimeMillis();
+    private String price = "0";
     private String returnUrl = "https://7a3af19f4d4c.ngrok-free.app/api/payment/cards/inicis/return";
     private String closeUrl = "https://7a3af19f4d4c.ngrok-free.app/card/return";
     private String signKey = "SU5JTElURV9UUklQTEVERVNfS0VZU1RS";
@@ -52,16 +54,20 @@ public class InicisService {
     private String offerPeriod = ""; // 필요 없으면 빈값 처리
 
     /**
-     * PG 팝업창 오픈 파라미터 생성
+     * PG(이니시스) 팝업창 오픈 파라미터
      * @param oid
-     * @param buyerName
-     * @param buyerTel
-     * @param buyerEmail
-     * @param goodName
+     * @param request
      * @return
      */
-    public InicisBillingAuthResponse createBillingAuthParams(String oid, String buyerName, String buyerTel, String buyerEmail, String goodName) {
+    public InicisBillingAuthResponse createBillingAuthParams(String oid, CardAuthRequest request){
+
+        String buyerName = request.getBuyerName();
+        String buyerTel = request.getBuyerTel();
+        String buyerEmail = request.getBuyerEmail();
+        String goodName = request.getGoodName();
+
         // ※ 해시 생성 공식은 반드시 이니시스 문서 기준으로 맞춰야 함!
+        String timestamp = idGeneratorUtil.timestampGenerate();
         String signatureText = String.format("oid=%s&price=%s&timestamp=%s", oid, price, timestamp);
         String signature = HashUtils.sha256(signatureText);
 
@@ -110,24 +116,12 @@ public class InicisService {
             // 이미 처리됨 – 멱등
             log.info("이미 ACTIVE. oid={}", oid);
             return null;
-            // 이미 저장된 데이터를 기반으로 응답을 재구성할 수도 있으나,
-            // 여기선 외부 응답을 그대로 반환하기 어렵기 때문에 최소 메시지 로그로 마무리.
-            // 필요 시 PaymentUserCard → Response 매핑 추가.
-//            return InicisBillingKeyResponse.builder()
-//                    .mid(props.getMid())
-//                    .MOID(oid)
-//                    .CARD_BillKey(pending.getBillingKey())
-//                    .CARD_Num(pending.getCardNumberMasked())
-//                    .CARD_PurchaseCode(pending.getCardCompany())
-//                    .payMethodDetail(pending.getCardType())
-//                    .build();
         }
 
         String authUrl = request.getAuthUrl();
-        log.info(authUrl);
         mid = request.getMid();
         String authToken = request.getAuthToken();
-        timestamp = System.currentTimeMillis();
+        String timestamp = idGeneratorUtil.timestampGenerate();
 
         String signatureText = String.format("authToken=%s&timestamp=%s", authToken, timestamp);
         String signature = HashUtils.sha256(signatureText);
