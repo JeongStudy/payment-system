e04f8fe3212b7efa2c6d3cfd14f78c3ef1bc8d66
jaebin1234 <jaebin1234>
2025-08-12 21:49:01 +0900

    fix : AesKey, PaymentUser 줄바꿈 수정

--- src/main/java/com/system/payment/user/domain/PaymentUser.java
+++ src/main/java/com/system/payment/user/domain/PaymentUser.java
@@ -12,40 +12,40 @@ import lombok.NoArgsConstructor;
 @Getter
 public class PaymentUser extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-   @Column(length = 200, nullable = false, unique = true)
-    private String email;
+	@Column(length = 200, nullable = false, unique = true)
+	private String email;
 
-    @Column(length = 300, nullable = false)
-    private String password;
+	@Column(length = 300, nullable = false)
+	private String password;
 
-    @Column(length = 50, nullable = false)
-    private String lastName;
+	@Column(length = 50, nullable = false)
+	private String lastName;
 
-    @Column(length = 50, nullable = false)
-    private String firstName;
+	@Column(length = 50, nullable = false)
+	private String firstName;
 
-    @Column(length = 11, nullable = false)
-    private String phoneNumber;
+	@Column(length = 11, nullable = false)
+	private String phoneNumber;
 
-    private PaymentUser(String email, String encodedPassword, String firstName, String lastName, String phoneNumber) {
+	private PaymentUser(String email, String encodedPassword, String firstName, String lastName, String phoneNumber) {
 		this.email = email;
-        this.password = encodedPassword;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
+		this.password = encodedPassword;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
 	}
 
-    public static PaymentUser create(String email, String encodedPassword, String firstName, String lastName, String phoneNumber) {
-        return new PaymentUser(
+	public static PaymentUser create(String email, String encodedPassword, String firstName, String lastName, String phoneNumber) {
+		return new PaymentUser(
 				email,
 				encodedPassword,
 				firstName,
 				lastName,
 				phoneNumber
 		);
-    }
+	}
 }
