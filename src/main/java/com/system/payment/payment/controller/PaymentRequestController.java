99a52c651c37854500201ffb743678f31382d80b
jaebin1234 <jaebin1234>
2025-08-14 19:13:18 +0900

    fix : PaymentRequestController 위치 수정

--- src/main/java/com/system/payment/payment/controller/PaymentRequestController.java
+++ src/main/java/com/system/payment/payment/controller/PaymentRequestController.java
@@ -16,24 +16,19 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class PaymentRequestController {
 
-    private final PaymentRequestService paymentRequestService;
+	private final PaymentRequestService paymentRequestService;
 
-    @PostMapping("/requests")
-    public ResponseEntity<Response<CreatePaymentResponse>> create(
-            @Valid @RequestBody CreatePaymentRequest request
-    ) {
-        final CreatePaymentResponse createPaymentResponse = paymentRequestService.createAndPublish(request);
-        return Response.created(createPaymentResponse, SuccessCode.PAYMENT_REQUESTS_SUCCESS);
-    }
 	@GetMapping("/requests/idempotency-key")
 	public ResponseEntity<Response<IdempotencyKeyResponse>> getIdempotencyKey() {
         final IdempotencyKeyResponse idempotencyKeyResponse = paymentRequestService.getIdempotencyKey();
         return Response.ok(idempotencyKeyResponse,SuccessCode.PAYMENT_IDEMPOTENCY_KEY_SUCCESS);
 	}
 
-//    @GetMapping("/requests/{id}")
-//    public ResponseEntity<Response<PaymentRequestResponse>> get(@PathVariable Long id) {
-//        PaymentRequest pr = paymentRequestService.getByIdOrThrow(id);
-//        return ResponseEntity.ok(Response.ok(PaymentRequestResponse.from(pr)));
-//    }
+	@PostMapping("/requests")
+	public ResponseEntity<Response<CreatePaymentResponse>> createPaymentAndPublish(
+			@Valid @RequestBody CreatePaymentRequest request
+	) {
+		final CreatePaymentResponse createPaymentResponse = paymentRequestService.createPaymentAndPublish(request);
+		return Response.created(createPaymentResponse, SuccessCode.PAYMENT_REQUESTS_SUCCESS);
+	}
 }
