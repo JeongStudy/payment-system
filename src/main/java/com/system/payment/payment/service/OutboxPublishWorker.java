2fae53754414944db814f539c645f8f4e53b2df9
jaebin1234 <43032589+jaebin1234@users.noreply.github.com>
2025-08-18 15:50:29 +0900

    fix : 주입 객체 네이밍 변경

--- src/main/java/com/system/payment/payment/service/OutboxPublishWorker.java
+++ src/main/java/com/system/payment/payment/service/OutboxPublishWorker.java
@@ -14,24 +14,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class OutboxPublishWorker {
-	private final OutboxEventRepository repo;
+	private final OutboxEventRepository outboxEventRepository;
 	private final OutboxService outboxService;
 	private final PaymentRepository paymentRepository;
-	private final PaymentUserCardRepository cardRepo;
+	private final PaymentUserCardRepository paymentUserCardRepository;
 	private final UserService userService;
 	private final PaymentProducer paymentProducer;
 	private final ObjectMapper mapper;
 
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void processOne(Long eventId) throws Exception {
-		var e = repo.findByIdForUpdate(eventId).orElseThrow();
+		var e = outboxEventRepository.findByIdForUpdate(eventId).orElseThrow();
 		if (!"PENDING".equals(e.getStatus())) return;
 		if (!"PAYMENT_REQUESTED_V1".equals(e.getEventType())) return;
 
 		var args = mapper.readValue(e.getPayload(), PaymentRequestedArgs.class);
 		var payment = paymentRepository.findById(args.paymentId()).orElseThrow();
-		var user = userService.findUser();                  // 필요 시 메서드 조정
-		var card = cardRepo.findById(args.methodId()).orElseThrow();
+		var user = userService.findUser();
+		var card = paymentUserCardRepository.findById(args.methodId()).orElseThrow();
 
 		paymentProducer.sendPaymentRequested(payment, user, card, args.productName());
 		outboxService.markSent(e);
