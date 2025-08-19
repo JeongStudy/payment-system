abb0bf77a75f60d3c1cdc814d6b71dc15940475d
jaebin1234 <jaebin1234>
2025-08-19 14:55:25 +0900

    fix : 매개변수 대문자를 소문자로 변경

--- src/main/java/com/system/payment/payment/repository/PaymentRepository.java
+++ src/main/java/com/system/payment/payment/repository/PaymentRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-	boolean existsByIdempotencyKey(String IdempotencyKey);
-	Optional<Payment> findByIdempotencyKey(String IdempotencyKey);
+	boolean existsByIdempotencyKey(String idempotencyKey);
+	Optional<Payment> findByIdempotencyKey(String idempotencyKey);
 }
