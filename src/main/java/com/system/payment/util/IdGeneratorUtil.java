c5e1c244022f13824f35d57cd10204fdd5c5a6db
정윤환 <629jyh7@naver.com>
2025-08-22 16:09:27 +0900

    add: UUID 생성 Util 메서드 추가

--- src/main/java/com/system/payment/util/IdGeneratorUtil.java
+++ src/main/java/com/system/payment/util/IdGeneratorUtil.java
@@ -2,11 +2,15 @@ package com.system.payment.util;
 
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class IdGeneratorUtil {
     public String oidGenerate() {
         return "DemoTest_" + System.currentTimeMillis();
     }
+
+    public String UUIDGenerate() { return UUID.randomUUID().toString(); }
 
     public String timestampGenerate() {
         return String.valueOf(System.currentTimeMillis());
