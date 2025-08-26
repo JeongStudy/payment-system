71fb3ce909ee827bfd53bb87bc367c52fbc480df
jaebin1234 <jaebin1234>
2025-08-27 02:06:12 +0900

    fix : EventType Enum으로 변경

--- src/main/java/com/system/payment/payment/domain/outbox/OutboxEvent.java
+++ src/main/java/com/system/payment/payment/domain/outbox/OutboxEvent.java
@@ -21,19 +21,27 @@ public class OutboxEvent extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
 	@Column(nullable = false)
-	private String eventType;     // e.g. "PAYMENT_REQUESTED_V1"
+	@Enumerated(EnumType.STRING)
+	private EventType eventType;
+
 	@Column(nullable = false)
 	private String eventKey;      // e.g. transactionId (카프카 key 겸 멱등키)
+
 	@Column(nullable = false, columnDefinition = "jsonb")   // RDB별 타입 조정
 	@JdbcTypeCode(SqlTypes.JSON)
 	private String payload;                               // JSON(아래 Args)
+
 	@Column(nullable = false)
 	private String status;        // PENDING/SENT/FAILED
+
 	@Column(nullable = false)
 	private Integer attempts;     // 재시도 횟수
+
 	@Column(nullable = false)
 	private LocalDateTime nextAttemptAt; // 재시도 타이밍을 제어하는 스케줄 타임스탬프
+
 	@Column(columnDefinition = "text")
 	private String lastError; // 재시도 타이밍을 제어하는 스케줄 타임스탬프
 
