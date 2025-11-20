e4f12abf687fbbae8597a0abecb519c43ac3df1c
jaebin1234 <jaebin1234>
2025-11-20 18:56:20 +0900

    fix : Param 어노테이션 이슈 수정

--- src/main/java/com/system/payment/payment/repository/PaymentRepository.java
+++ src/main/java/com/system/payment/payment/repository/PaymentRepository.java
@@ -5,6 +5,7 @@ import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -14,5 +15,5 @@ public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("select p from Payment p where p.id = :id")
-	Optional<Payment> findByIdForUpdate(Integer id);
+	Optional<Payment> findByIdForUpdate(@Param("id") Integer id);
 }
