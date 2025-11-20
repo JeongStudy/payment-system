cc29ea599e4fe96ad901a72f0d8c27e290f68cb6
jaebin1234 <43032589+jaebin1234@users.noreply.github.com>
2025-11-20 13:49:14 +0900

    fix: BaseEntity 위치 수정

--- src/main/java/com/system/payment/payment/domain/PaymentDetail.java
+++ src/main/java/com/system/payment/payment/domain/PaymentDetail.java
@@ -1,6 +1,6 @@
 package com.system.payment.payment.domain;
 
-import com.system.payment.common.domain.BaseEntity;
+import com.system.payment.common.domain.entity.BaseEntity;
 import com.system.payment.payment.domain.converter.PaymentResultCodeConverter;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
--- src/main/java/com/system/payment/payment/domain/PaymentIdempotency.java
+++ src/main/java/com/system/payment/payment/domain/PaymentIdempotency.java
@@ -1,6 +1,6 @@
 package com.system.payment.payment.domain;
 
-import com.system.payment.common.domain.BaseEntity;
+import com.system.payment.common.domain.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
